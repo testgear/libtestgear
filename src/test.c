2654057fe6116bec424ce5121702fb985b1f0a2c
Martin Lund <martin.lund@keep-it-simple.com>
2014-10-09 22:18:49 +0200

    Minor cleanup of test.c

--- src/test.c
+++ src/test.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         printf("Usage: %s <ip>\n", argv[0]);
-        exit(1);
+        return 0;
     }
 
     // Connect to test gear device server
